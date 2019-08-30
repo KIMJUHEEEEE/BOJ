738931bafacaf4306439277aba0a1d681a5b7e41
KIMJUHEEEEE <jhwngml97@gmail.com>
2019-08-30 13:41:42 +0900

    BOJ #1037

---
+++ 1037.cpp
@@ -0,0 +1,21 @@
+﻿#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int main() {
+	int ca;
+	cin >> ca;
+	int a[51];
+	int n = 0;
+	for (int i = 0; i < ca; i++)
+	{
+		cin >> a[i];
+	}
+	sort(a, a + ca);
+	if (ca % 2 == 0)
+		n = a[ca / 2 - 1] * a[ca/2];
+	else
+		n = a[ca / 2] * a[ca / 2];
+	cout << n;
+}
