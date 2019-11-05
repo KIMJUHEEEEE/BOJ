1583931684a3a0394eff3e417ab6031f4f4b0c53
KIMJUHEEEEE <jhwngml97@gmail.com>
2019-11-05 22:18:22 +0900

    피보나치수5

---
+++ 10870.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int arr[21];
+int fib(int n) {
+	arr[1] = 1;
+	for (int i = 2; i < 21; i++)
+	{
+		arr[i] = arr[i - 1] + arr[i - 2];
+	}
+	return arr[n];
+}
+int main() {
+	int n = 0;
+	cin >> n;
+	cout << fib(n);
+}
